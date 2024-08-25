3cd9ca3fc1e7a4dda445f22b10d9081e77ef253c
PrathamRawat <prathamrawat2004@gmail.com>
2024-08-25 13:39:56 -0700

    Updated database

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-const MongoDbURI = "mongodb://localhost:27017/hospital";
+const MongoDbURI = "mongodb+srv://prathamrawat2004:DOSwxk66FVQV0Xf6@cluster0.kb3ux.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 import usersRoute from "./Routes/Users.route.js";
 import cors from "cors";
 
@@ -15,10 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 // connecting to db
-mongoose.connect(MongoDbURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(()=>{
+mongoose.connect(MongoDbURI).then(()=>{
     console.log("Connected to database");
 }).catch(()=>{
     console.log("Not connected");
